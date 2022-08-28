2ac621bcec09e41f74b5e397ee67baa8412358d3
KarlP17123 <karl.park@telia.com>
2022-08-28 20:41:17 +0000

    Add the city Questions

---
+++ assets/js/questions.js
@@ -0,0 +1,80 @@
+let MCQS = [{
+    question: "Which city in USA has the most population?",
+    choice1: "Los Angeles",
+    choice2: "Chicago",
+    choice3: "New York",
+    choice4: "San Diego",
+    answer: 3
+},           
+            {
+            question: "How many people live in New York?",
+    choice1: "8 million",
+    choice2: "20 million",
+    choice3: "8.5  million",
+    choice4: "5 million",
+    answer: 3 
+},          
+            {
+            question: "Which city is the world's largest by area?",
+    choice1: "Tokyo",
+    choice2: "Maxico City",
+    choice3: "London",
+    choice4: "New York",
+    answer: 1 
+},
+             {
+             question: "Which city is the largest in Germany?",
+    choice1: "München",
+    choice2: "Berlin",
+    choice3: "Hamburg",
+    choice4: "Köln",
+    answer: 2
+},           
+             {
+             question: "Which city in Europe is the most expensive to live in?",
+    choice1: "Berlin",
+    choice2: "London",
+    choice3: "Zürich",
+    choice4: "Genève",
+    answer: 3
+},
+             {
+             question: "Which city in the world has the most tourists?",
+    choice1: "London",
+    choice2: "Tokyo",
+    choice3: "Bangkok",
+    choice4: "Dubai",
+    answer: 3
+},   
+             {
+             question: "Which city in the world has the most hours of sunshine?",
+    choice1: "Yuma",
+    choice2: "London",
+    choice3: "Paris",
+    choice4: "Miami",
+    answer: 1
+},
+             {
+             question: "Which city is the capital of France?",
+    choice1: "Tokyo",
+    choice2: "Nice",
+    choice3: "Marseille",
+    choice4: "Paris",
+    answer: 4
+},
+             {
+             question: "Which city in europe has the most population?",
+    choice1: "Sankt Petersburg",
+    choice2: "Moskva",
+    choice3: "Paris",
+    choice4: "London",
+    answer: 2
+},
+             {
+             question: "Which city has the most rainy days?",
+    choice1: "Cherrapunijee",
+    choice2: "Tokyo",
+    choice3: "London",
+    choice4: "New York",
+    answer: 1
+}];
--- assets/js/script.js
+++ assets/js/script.js
@@ -49,3 +49,50 @@ start.addEventListener("click" , ()=>{
     start.style.display = "none";
     Information.style.display = "block";
 });
+
+//what happen when 'Exit' Button Will Click
+exit.addEventListener("click" , ()=>{
+    start.style.display = "block";
+    Information.style.display = "none";
+});
+
+
+//Creating Timer For Quiz Timer Section
+
+let countDown = ()=>{
+    if(timer === 20)
+    {
+        clearInterval(interval);
+        next_question.click();
+    }
+    else
+        {
+             timer++;
+             time.innerText = timer;
+        }
+}
+
+//setInterval(countDown,1000);
+
+let loadData = ()=>{
+    questionNo.innerText = index + 1 + ". ";
+    questionText.innerText = MCQS[index].question;
+    option1.innerText = MCQS[index].choice1;
+    option2.innerText = MCQS[index].choice2;
+    option3.innerText = MCQS[index].choice3;
+    option4.innerText = MCQS[index].choice4;
+
+    //timer start
+    timer = 0;
+}
+
+loadData();
+
+//what happen when 'Continue' Button Will Click
+continueBtn.addEventListener("click" , ()=>{
+    quiz.style.display = "block";
+    Information.style.display = "none";
+
+    interval = setInterval();
+    loadData();
+});
